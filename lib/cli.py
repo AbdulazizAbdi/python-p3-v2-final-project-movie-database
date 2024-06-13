3a1948802847d67cbc32b5bb309902e0a09938e0
Abdulaziz Abdi <amigf76@gmail.com>
2024-06-13 17:56:15 +0300

    second commit

--- lib/cli.py
+++ lib/cli.py
@@ -2,7 +2,19 @@
 
 from helpers import (
     exit_program,
-    helper_1
+    list_genres,
+    find_genre_by_name,
+    find_genre_by_id,
+    create_genre,
+    update_genre,
+    delete_genre,
+    list_movies,
+    find_movie_by_title,
+    find_movie_by_id,
+    create_movie,
+    update_movie,
+    delete_movie,
+    list_of_genres_movies
 )
 
 
@@ -13,15 +25,55 @@ def main():
         if choice == "0":
             exit_program()
         elif choice == "1":
-            helper_1()
+            list_genres()
+        elif choice == "2":
+            find_genre_by_id()
+        elif choice == "3":
+            find_genre_by_name()
+        elif choice == "4":
+            create_genre()
+        elif choice == "5":
+            update_genre()
+        elif choice == "6":
+            delete_genre()
+        elif choice == "7":
+            list_movies()
+        elif choice == "8":
+            find_movie_by_id()
+        elif choice == "9":
+            find_movie_by_title()
+        elif choice == "10":
+            create_movie()
+        elif choice == "11":
+            update_movie()
+        elif choice == "12":
+            delete_movie()
+        elif choice == "13":
+            list_of_genres_movies()
         else:
             print("Invalid choice")
 
 
 def menu():
-    print("Please select an option:")
+    print("MOVIE DATABASE MANAGER")
+    print("Genre Options")
+    print("Please select an Genre option from 1 - 6:")
     print("0. Exit the program")
-    print("1. Some useful function")
+    print("1. Genre List")
+    print("2. Find Genre by id")
+    print("3. Find Genre by name")
+    print("4: Create new genre")
+    print("5: Update existing genre")
+    print("6: Delete genres")
+    print("Movie Options")
+    print("Please select an Movie option from 7 - 13:")
+    print("7. Movie List")
+    print("8. Find Movie by id")
+    print("9. Find Movie by title")
+    print("10: Create new movie")
+    print("11: Update existing movie")
+    print("12: Delete movie")
+    print("13: List all movies in a genre")
 
 
 if __name__ == "__main__":
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,9 +1,141 @@
 # lib/helpers.py
 
-def helper_1():
-    print("Performing useful function#1.")
+from models.genre import Genre
+from models.movie import Movie
 
 
 def exit_program():
     print("Goodbye!")
     exit()
+
+def list_genres():
+    genres = Genre.get_all()
+    for genre in genres:
+        print(genre)
+
+
+def find_genre_by_name():
+    name = input("Please enter the genre's name: ")
+    genre = Genre.find_by_name(name)
+    print(genre) if genre else print(
+        f'This Genre: {name} is not available')
+
+
+def find_genre_by_id():
+    id_ = input("Please enter the genre's id: ")
+    genre = Genre.find_by_id(id_)
+    print(genre) if genre else print(f'This Genre: {id_} is not available')
+
+
+def create_genre():
+    name = input("Please enter the genre's name: ")
+    description = input("Please enter the genre's description: ")
+    try:
+        genre = Genre.create(name, description)
+        print(f'Genre created: {genre}')
+    except Exception as exc:
+        print("Unable to create new genre: ", exc)
+
+
+def update_genre():
+    id_ = input("Please enter the genre's id: ")
+    if genre := Genre.find_by_id(id_):
+        try:
+            name = input("Please enter the genre's updated name: ")
+            genre.name = name
+            description = input("Please enter the genre's updated description: ")
+            description.location = location
+
+            genre.update()
+            print(f'Genre updated: {genre}')
+        except Exception as exc:
+            print("Unable to update genre: ", exc)
+    else:
+        print(f'This Genre: {id_} is not available')
+
+
+def delete_genre():
+    id_ = input("Please enter the genre's id: ")
+    if genre := Genre.find_by_id(id_):
+        genre.delete()
+        print(f'This Genre: {id_} has been deleted')
+    else:
+        print(f'This Genre: {id_} is not available')
+
+def list_movies():
+    movies = Movie.get_all()
+    for movie in movies:
+        print(movie)
+
+
+def find_movie_by_title():
+    title = input("Please enter the movie's title: ")
+    movie = Movie.find_by_title(title)
+    print(movie) if movie else print(
+        f'This Movie: {name} is not available')
+
+
+def find_movie_by_id():
+    id_ = input("Please enter the movie's id: ")
+    movie = Movie.find_by_id(id_)
+    print(movie) if movie else print(f'This Movie: {id_} is not available')
+
+
+def create_movie():
+    title = input("Please enter the new movie's title: ")
+    runtime = input("Please enter the new movie's runtime: ")
+    age_rating = input("Please enter the new movie's age rating: ")
+    genre_id = input("Please enter the new movie's genre id: ")
+    try:
+        movie = Genre.create(title, runtime, age_rating, genre_id)
+        print(f'Movie created: {movie}')
+    except Exception as exc:
+        print("Unable to create new movie: ", exc)
+
+
+def update_movie():
+    id_ = input("Please enter the movie's id: ")
+    if movie := Movie.find_by_id(id_):
+        try:
+            title = input("Please enter the new movie's title: ")
+            movie.title = title
+            runtime = input("Please enter the new movie's runtime: ")
+            movie.runtime = runtime
+            age_rating = input("Please enter the new movie's age rating: ")
+            movie.age_rating = age_rating
+            genre_id = input("Please enter the new movie's genre id: ")
+            movie.genre_id = genre_id
+
+            movie.update()
+            print(f'Movie Updated: {Movie}')
+        except Exception as exc:
+            print("Unable to update movie: ", exc)
+    else:
+        print(f'This Movie: {id_} is not available')
+
+
+def delete_movie():
+    id_ = input("Please enter the movie's id: ")
+    if movie := Movie.find_by_id(id_):
+        movie.delete()
+        print(f'This Movie: {id_} has been deleted')
+    else:
+        print(f'This Movie: {id_} is not available')
+
+def list_of_genres_movies():
+    id_ = input("Please enter the genre's id: ")
+    id_ = int(id_)
+    if genre := Genre.find_by_id(id_):
+        try:
+            movies_list = []
+            movies = Movie.get_all()
+            for movie in movies:
+                if movie.department_id == id_:
+                    movie_list.append(movie)
+
+            print(movie_list)
+        except:
+            print("No movies's are in this genre")
+        
+    else:
+        print(f'This Movie: {id_} is not available')
